c0c3d3ed53b12336e9fa6ef54e858d88dcafdbe1
Giovana de Miranda <giovana.miranda@bitize.com.br>
2023-11-29 14:04:00 -0300

    arrumando alinhamento do form do reservas

--- src/components/Reservations/Form.jsx
+++ src/components/Reservations/Form.jsx
@@ -4,14 +4,14 @@ import { toast } from 'react-toastify';
 import styled from 'styled-components';
 
 const FormContainer = styled.form`
-  max-width: 1024px;
+  max-width: 1200px;
   margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
   align-items: flex-end;
   justify-content: flex-end;
   gap: 20px 60px;
-  padding: 20px 100px;
+  padding: 20px 241px;
   background-color: #16161a;
   box-shadow: 0px 0px 2px #ccc;
   border-radius: 5px;
@@ -20,6 +20,26 @@ const FormContainer = styled.form`
     flex-direction: column;
     column-gap: 20px;
   }
+`;
+
+const LineUp = styled.div`
+  display: flex;
+  gap: 59px;
+  align-items: start;
+  justify-content: start;
+  margin-bottom: 16px;
+`;
+
+const LineDown = styled.div`
+  display: flex;
+  gap: 59px;
+  width: 100%;
+`;
+
+const LineWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
 
 const InputArea = styled.div`
@@ -53,7 +73,8 @@ const Label = styled.label``;
 
 const Button = styled.button`
   flex: 1 0 2.5rem;
-  padding: 0 2.5rem;
+  padding: 0 10px;
+  width: 200px;
   cursor: pointer;
   border-radius: 5px;
   border: none;
@@ -170,73 +191,79 @@ const Form = ({ getReservations, clients, rooms, onEdit, setOnEdit }) => {
 
   return (
     <FormContainer onSubmit={handleSubmit} ref={ref}>
-      <InputArea>
-        <Label>Nome do cliente</Label>
-        <Select
-          name="id_cliente"
-          value={selectedCliente}
-          onChange={(e) => onClienteChange(e.target.value)}
-        >
-          {clients && clients.map((cliente) => (
-            <option key={cliente.id} value={cliente.id}>
-              {cliente.nome}
-            </option>
-          ))}
-        </Select>
-      </InputArea>
-      <InputArea>
-        <Label>Número do quarto</Label>
-        <Select
-          name="id_quarto"
-          value={selectedQuarto}
-          onChange={(e) => onQuartoChange(e.target.value)}
-        >
-          {rooms && rooms.map((room) => (
-            <option key={room.id} value={room.id}>
-              {room.id}
-            </option>
-          ))}
-        </Select>
-      </InputArea>
-      <InputArea>
-        <Label>Data de entrada</Label>
-        <Input
-          placeholder="Data da entrada"
-          name="data_entrada"
-          type="date"
-          value={reservation.data_entrada}
-          onChange={(e) => setReservation({ ...reservation, data_entrada: e.target.value })}
-        />
-      </InputArea>
-      <InputArea>
-        <Label>Data da saída</Label>
-        <Input
-          placeholder="Data de saída"
-          name="data_saida"
-          type="date"
-          value={reservation.data_saida}
-          onChange={(e) => setReservation({ ...reservation, data_saida: e.target.value })}
-        />
-      </InputArea>
-      <InputArea>
-        <Label>Forma de pagamento</Label>
-        <Select
-          name="forma_pagamento"
-          value={selectedPagamento}
-          onChange={(e) => onPagamentoChange(e.target.value)}
-        >
-          <option value="" disabled hidden>
-            Escolha uma opção
-          </option>
-          <option value="2">Dinheiro</option>
-          <option value="1">Cartão de Crédito</option>
-          <option value="0">Cartão de Débito</option>
-        </Select>
-      </InputArea>
-      <InputArea>
-        <LabelButton>.</LabelButton>
-        <Button type="submit">SALVAR</Button>
-      </InputArea>
+      <LineWrapper>
+        <LineUp>
+          <InputArea>
+            <Label>Nome do cliente</Label>
+            <Select
+              name="id_cliente"
+              value={selectedCliente}
+              onChange={(e) => onClienteChange(e.target.value)}
+            >
+              {clients && clients.map((cliente) => (
+                <option key={cliente.id} value={cliente.id}>
+                  {cliente.nome}
+                </option>
+              ))}
+            </Select>
+          </InputArea>
+          <InputArea>
+            <Label>Número do quarto</Label>
+            <Select
+              name="id_quarto"
+              value={selectedQuarto}
+              onChange={(e) => onQuartoChange(e.target.value)}
+            >
+              {rooms && rooms.map((room) => (
+                <option key={room.id} value={room.id}>
+                  {room.id}
+                </option>
+              ))}
+            </Select>
+          </InputArea>
+          <InputArea>
+            <Label>Data de entrada</Label>
+            <Input
+              placeholder="Data da entrada"
+              name="data_entrada"
+              type="date"
+              value={reservation.data_entrada}
+              onChange={(e) => setReservation({ ...reservation, data_entrada: e.target.value })}
+            />
+          </InputArea>
+        </LineUp>
+        <LineDown>
+          <InputArea>
+            <Label>Data da saída</Label>
+            <Input
+              placeholder="Data de saída"
+              name="data_saida"
+              type="date"
+              value={reservation.data_saida}
+              onChange={(e) => setReservation({ ...reservation, data_saida: e.target.value })}
+            />
+          </InputArea>
+          <InputArea>
+            <Label>Forma de pagamento</Label>
+            <Select
+              name="forma_pagamento"
+              value={selectedPagamento}
+              onChange={(e) => onPagamentoChange(e.target.value)}
+            >
+              <option value="" disabled hidden>
+                Escolha uma opção
+              </option>
+              <option value="2">Dinheiro</option>
+              <option value="1">Cartão de Crédito</option>
+              <option value="0">Cartão de Débito</option>
+            </Select>
+          </InputArea>
+          <InputArea>
+            <LabelButton>.</LabelButton>
+            <Button type="submit">SALVAR</Button>
+          </InputArea>
+        </LineDown>
+      </LineWrapper>
     </FormContainer>
   );
 };
